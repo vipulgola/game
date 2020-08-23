34513b87f591f55026a8da59d30e733e14c1c62b
vipulgola <64086897+vipulgola@users.noreply.github.com>
2020-08-23 13:53:35 +0530

    Update event.js

--- event.js
+++ event.js
@@ -18,3 +18,37 @@ re.addEventListener('click',function(){
  for(var i = 0; i < sq.length;  i++)
  sq[i].textContent = '';
 })
+function complete(){
+  var c=0;
+  for(var i=0;i<9;i++){
+    sq[i].textContent='';
+    c = c+1;
+  }
+  if(c==0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+function check(){
+  if(sq[0]===sq[1] || sq[1]===sq[2])
+  {return sq[1];}
+  if(sq[0]===sq[3] || sq[3]===sq[6])
+  {return sq[3];}
+  if (sq[2]===sq[5] || sq[5]===sq[8]) {
+    return sq[5];
+  }
+  if(sq[6]===sq[7] || sq[7]===sq[8])
+  {return sq[7];}
+  if (sq[2]===sq[4] || sq[4]===sq[6]) {
+    return sq[4];
+  }
+  if(sq[0]===sq[4] || sq[4]===sq[8])
+  {return sq[4];}
+}
+var r = check();
+if(complete){
+  alert(r+"WINS");
+}
+
